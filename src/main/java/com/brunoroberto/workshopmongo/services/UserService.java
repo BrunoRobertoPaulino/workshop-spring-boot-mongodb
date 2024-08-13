47164d4cda91ee2d47ff44c901daad53eb13d41c
Bruno Roberto <brunoroberto.paulino@gmail.com>
2024-08-13 20:27:35 -0300

    Obtendo um usuário por id

--- src/main/java/com/brunoroberto/workshopmongo/services/UserService.java
+++ src/main/java/com/brunoroberto/workshopmongo/services/UserService.java
@@ -1,21 +1,28 @@
 package com.brunoroberto.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.brunoroberto.workshopmongo.domain.User;
 import com.brunoroberto.workshopmongo.repository.UserRepository;
+import com.brunoroberto.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
 }
