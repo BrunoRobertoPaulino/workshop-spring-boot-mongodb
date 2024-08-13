8a74a4c4f24b663e3282ce4f9228b0e8b8ccaad5
Bruno Roberto <brunoroberto.paulino@gmail.com>
2024-08-13 20:43:17 -0300

    Inserção de usuário com POST

--- src/main/java/com/brunoroberto/workshopmongo/services/UserService.java
+++ src/main/java/com/brunoroberto/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.brunoroberto.workshopmongo.domain.User;
+import com.brunoroberto.workshopmongo.dto.UserDTO;
 import com.brunoroberto.workshopmongo.repository.UserRepository;
 import com.brunoroberto.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,4 +26,11 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
